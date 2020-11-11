302ccb203d83c5f4c2738c21714ce5696293861c
unknown <jucode22@gmail.com>
2020-11-10 19:27:54 -0500

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,15 @@
 import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
-
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -17,4 +17,18 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+//we created a selector
+// because we wrote code that gets the entire state object and then pulls off a small portion or a slice of that state
+// we're computing a new value based on the state
+const mapStateToProps = (state) => {
+  return {
+    itemCount: selectCartItemsCount(state),
+  };
+};
+
+//whenever we return a new object, redux rebuilds the entire state object, so mapStatetoprops is always being called and
+// that means it is always rerendering but we dont always want our app to rerender a new quantity all the time
+// so we can actually store or cache the value of what our selector is using to compute its value using reselect
+// RESELECT DEFINITION: Reselect is a library for building memoized selectors. We define selectors as the functions that retrieve snippets of the Redux state for our React components. Using memoization, we can prevent unnecessary rerenders and recalculations of derived data which in turn will speed up our application
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -3,3 +3,20 @@ import { createSelector } from "reselect";
 //input selector
 // is a function that gets a whole state and only returns a slice of it, one layer deep
 const selectCart = (state) => state.cart;
+
+//cart is the object from the cart reducer
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
